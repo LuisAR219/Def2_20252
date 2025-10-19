3907702de7faf34dba355edf7de60aea39c6b50e
LuisAR219 <luis.alzate3@udea.edu.co>
2025-10-19 16:18:45 -0500

    Creación clase anuncio

---
+++ Def2/Def2_2025/anuncio.h
@@ -0,0 +1,16 @@
+#ifndef ANUNCIO_H
+#define ANUNCIO_H
+
+#include <string>
+
+class Anuncio {
+private:
+    int id;
+    std::string categoria;
+    std::string mensaje;
+
+public:
+
+};
+
+#endif // ANUNCIO_H
